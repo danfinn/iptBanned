eed329273a204b25e54c4edb082a5696e96cf057
Dan Finn <dan.finn@plansource.com>
2020-09-17 16:25:18 -0600

    adding some error checking and http error response

--- main.go
+++ main.go
@@ -16,7 +16,11 @@ var chain string
 func getBanned(ipt *iptables.IPTables) []string {
 	rules, err := ipt.List("filter", chain)
 	if err != nil {
+		// Return error so that showBanned can gracefully serve 500 error
+		var e []string
 		fmt.Printf("List of %v chain failed failed: %v", chain, err)
+		e = append(e, "error")
+		return e
 	}
 
 	// Strip out IP addresses from iptables output
@@ -40,15 +44,19 @@ func showBanned(res http.ResponseWriter, req *http.Request) {
         }
 
         f2bRules := getBanned(ipt)
+	// Check for error retrieving iptables chain info
+	if f2bRules[0] == "error" {
+		http.Error(res, "Unable to list IPTables chain", 500)
+	} else {
+		tpl, err := template.ParseFiles("/usr/local/iptBanned/banned.gohtml")
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	tpl, err := template.ParseFiles("/usr/local/iptBanned/banned.gohtml")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = tpl.Execute(res, f2bRules)
-	if err != nil {
-		log.Fatalln(err)
+		err = tpl.Execute(res, f2bRules)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 }
 
